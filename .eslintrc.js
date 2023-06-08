32ed090ff2974592f3a7af253ecb1bcdd5c278d7
reivenIV <runyalen88@duck.com>
2023-06-08 19:25:40 +0200

    worki in progress onboarding

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "plugins": [
+    "json-format"
+  ],
+  "extends": ["plugin:json-format/recommended"],
+  "rules": {
+    "json-format/indent": ["error", 2],
+    "json-format/quotes": ["error", "double"]
+  }
+}
--- crypto.js
+++ crypto.js
@@ -1,4 +1,9 @@
-const crypto = require("crypto");
+const crypto = require('crypto');
+
+const fs = require("fs");
+const raw_data = fs.readFileSync("payloads/tremorData.json");
+const humanReadableData = JSON.parse(raw_data);
+
 
 // function checkString(secret, data) {
 //   let responsetoString;
@@ -18,37 +23,37 @@ const crypto = require("crypto");
 // console.log(checkString("123"));
 
 function hashWithSecret(data, secret) {
-  const algorithm = "aes-256-cbc";
-  const key = crypto.scryptSync(secret, "salt", 32);
+  const algorithm = 'aes-256-cbc';
+  const key = crypto.scryptSync(secret, 'salt', 32);
   const iv = crypto.randomBytes(16);
 
   const cipher = crypto.createCipheriv(algorithm, key, iv);
-  let encrypted = cipher.update(data, "utf8", "hex");
-  encrypted += cipher.final("hex");
+  let encrypted = cipher.update(data, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
 
-  const hashedData = iv.toString("hex") + encrypted;
+  const hashedData = iv.toString('hex') + encrypted;
   return hashedData;
 }
 
 function unhashWithSecret(hashedData, secret) {
-  const algorithm = "aes-256-cbc";
-  const key = crypto.scryptSync(secret, "salt", 32);
-  const iv = Buffer.from(hashedData.slice(0, 32), "hex");
+  const algorithm = 'aes-256-cbc';
+  const key = crypto.scryptSync(secret, 'salt', 32);
+  const iv = Buffer.from(hashedData.slice(0, 32), 'hex');
   const encrypted = hashedData.slice(32);
 
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
-  let decrypted = decipher.update(encrypted, "hex", "utf8");
-  decrypted += decipher.final("utf8");
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
 
   return decrypted;
 }
 
 // Example usage
-const secretToken = "mySecretToken";
-const dataToHash = "67";
+const secretToken = 'mySecretToken';
+const dataToHash = '67';
 
 const hashedData = hashWithSecret(dataToHash, secretToken);
-console.log("Hashed data:", hashedData);
+console.log('Hashed data:', hashedData);
 
 const unhashedData = unhashWithSecret(hashedData, secretToken);
-console.log("Unhashed data:", unhashedData);
+console.log('Unhashed data:', unhashedData);
